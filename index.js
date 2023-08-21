08eed0d1e725499d7e23bc1606cb1c31d0b3e563
jonny desktop <jon@uslms.com>
2023-08-21 16:42:59 -0600

    changed a lot around

--- index.js
+++ index.js
@@ -46,35 +46,80 @@ headerLogoConatiner.addEventListener('click', () => {
 var navHover = document.getElementById('nav-hover');
 var header = document.querySelector('.header');
 
+let lastScrollPosition = 0;
+let isMouseOverNav = false;  // Track if mouse is over the nav or nav-hover
+let isMouseOverHeader = false;  // Track if mouse is over the header
+
 navHover.addEventListener('mouseenter', function() {
+  isMouseOverNav = true;
   header.style.top = '0';
 });
 
 navHover.addEventListener('mouseleave', function() {
-  header.style.top = '-100%';
+  isMouseOverNav = false;
+  if (!isMouseOverHeader) {  // Only hide if mouse isn't over the header
+    header.style.top = '-100%';
+  }
 });
 
 header.addEventListener('mouseenter', function() {
+  isMouseOverHeader = true;
   header.style.top = '0';
 });
 
 header.addEventListener('mouseleave', function() {
-  header.style.top = '-100%';
+  isMouseOverHeader = false;
+  if (!isMouseOverNav) {  // Only hide if mouse isn't over nav-hover
+    header.style.top = '-100%';
+  }
 });
+
+window.addEventListener('scroll', function() {
+  let currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  // If scrolling up
+  if (lastScrollPosition > currentScrollPosition) {
+    header.style.top = '0';
+  } 
+  // If scrolling down and not hovering over nav elements
+  else if (!isMouseOverNav && !isMouseOverHeader) {
+    header.style.top = '-100%';
+  }
+
+  // Update last scroll position
+  lastScrollPosition = currentScrollPosition;
+});
+
+
+
 
 
 
 
 //Hide and display the stuff
 
-document.getElementById('toggleButton').addEventListener('click', function() {
-  const otherProjects = document.getElementById('otherProjects');
-  if (otherProjects.style.display === 'none') {
-      otherProjects.style.display = 'block';
-  } else {
-      otherProjects.style.display = 'none';
-  }
-});
+// document.getElementById('toggleButton').addEventListener('click', function() {
+//   const otherProjects = document.getElementById('otherProjects');
+//   if (otherProjects.style.display === 'none') {
+//       otherProjects.style.display = 'block';
+//   } else {
+//       otherProjects.style.display = 'none';
+//   }
+// });
 
 
+//This could clear the form
+// const formSubmitButton = document.getElementById('submitButton');
 
+// formSubmitButton.addEventListener('click', (event) => {
+//     // Get the form element
+//     const form = event.target.form;
+    
+//     // Optional: If you want to prevent the form from actually submitting (for testing)
+//     // event.preventDefault();
+
+//     // Clear all the input and textarea elements
+//     form.querySelectorAll('input[type="text"], textarea').forEach(input => {
+//         input.value = '';
+//     });
+// });
